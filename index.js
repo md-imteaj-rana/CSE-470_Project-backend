5f94d96c0481f6e38e27a7e9a95d84dd29f30636
Ahsanul <ahsanulfahim2002@gmail.com>
2026-05-04 00:47:00 +0600

    Add backend routes for cart and orders

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -30,39 +33,45 @@ async function run() {
     await client.connect();
 
     // All the backend wroks will start from here. 
+    // All the backend wroks will start from here. 
 
     // Creating database
     const database = client.db('CampusBazar-CSE470-DB')
 
+    // creating collections
+    const userCollections = database.collection('user');
 
-    // creating collections for users
-    const userCollections = database.collection('user')
+    // ================= FAHIM'S COLLECTIONS START =================
+    // Feature: My Cart and My Orders
+    const cartCollections = database.collection('cart');
+    const orderCollections = database.collection('orders');
+    // ================= FAHIM'S COLLECTIONS END =================
+
+
     // saving the user to db
     app.post('/users', async (req, res) => {
       const userInfo = req.body;
-      userInfo.role = "general user"
+      userInfo.role = "general user";
       userInfo.createdAt = new Date();
 
-
       const result = await userCollections.insertOne(userInfo);
-
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Getting role of the current user
     app.get('/users/role/:email', async (req, res) => {
-      const { email } = req.params
+      const { email } = req.params;
 
-      const query = { email: email }
-      const result = await userCollections.findOne(query)
-      res.send(result)
-    })
+      const query = { email: email };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
 
     // Get all users
     app.get('/users', async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
-    })
+    });
 
     // Update user role
     const { ObjectId } = require('mongodb');
@@ -70,21 +79,123 @@ async function run() {
     app.patch('/users/:id', async (req, res) => {
       const { id } = req.params;
       const { role } = req.body;
+
       const filter = { _id: new ObjectId(id) };
       const updateDoc = { $set: { role } };
+
       const result = await userCollections.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
 
     // Delete a user
     app.delete('/users/:id', async (req, res) => {
       const { id } = req.params;
+
       const filter = { _id: new ObjectId(id) };
       const result = await userCollections.deleteOne(filter);
-      res.send(result);
-    })
 
-    // DO NOT CHANGE THE CODE BELOW ...
+      res.send(result);
+    });
+
+
+    // ================= FAHIM'S PART START =================
+    // Feature: My Cart and My Orders backend
+
+
+    // ================= CART ROUTES - FAHIM =================
+
+    // Add item to cart
+    app.post('/cart', async (req, res) => {
+      const cartItem = req.body;
+
+      cartItem.createdAt = new Date();
+
+      const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // Get cart items by user email
+    app.get('/cart/:email', async (req, res) => {
+      const { email } = req.params;
+
+      const query = { email: email };
+      const result = await cartCollections.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // Update cart item quantity
+    app.patch('/cart/:id', async (req, res) => {
+      const { id } = req.params;
+      const { quantity } = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          quantity: quantity
+        }
+      };
+
+      const result = await cartCollections.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // Clear all cart items for a user
+    app.delete('/cart/clear/:email', async (req, res) => {
+      const { email } = req.params;
+
+      const query = { email: email };
+      const result = await cartCollections.deleteMany(query);
+
+      res.send(result);
+    });
+
+    // Remove single cart item
+    app.delete('/cart/:id', async (req, res) => {
+      const { id } = req.params;
+
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollections.deleteOne(filter);
+
+      res.send(result);
+    });
+
+
+    // ================= ORDER ROUTES - FAHIM =================
+
+    // Place order
+    app.post('/orders', async (req, res) => {
+      const orderInfo = req.body;
+
+      orderInfo.createdAt = new Date();
+      orderInfo.status = "pending";
+
+      const result = await orderCollections.insertOne(orderInfo);
+      res.send(result);
+    });
+
+    // Get orders by user email
+    app.get('/orders/:email', async (req, res) => {
+      const { email } = req.params;
+
+      const query = { email: email };
+      const result = await orderCollections.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // Delete order
+    app.delete('/orders/:id', async (req, res) => {
+      const { id } = req.params;
+
+      const filter = { _id: new ObjectId(id) };
+      const result = await orderCollections.deleteOne(filter);
+
+      res.send(result);
+    });
+
+    // ================= FAHIM'S PART END =================  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
