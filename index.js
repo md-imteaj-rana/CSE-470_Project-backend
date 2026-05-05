26d1c14e9a6ca595c639b9a6e6fec15ff6faf253
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2026-05-06 00:36:25 +0600

    final

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://campusbazar470:CampusBazarCse470@pawmarta10.t0jzost.mongodb.net/?appName=PawMartA10";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pawmarta10.t0jzost.mongodb.net/?appName=PawMartA10`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     // All the backend wroks will start from here. 
     // All the backend wroks will start from here. 
@@ -418,7 +418,7 @@ async function run() {
     // ================= FAHIM'S PART END =================  
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
